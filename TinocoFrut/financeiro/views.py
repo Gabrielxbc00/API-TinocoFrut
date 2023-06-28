8558e446b747b26942eedcda5d613f3117bf35b8
Gabriel Ximenes <117924325+Gabrielxbc00@users.noreply.github.com>
2023-06-28 09:31:08 -0300

    Update views.py

--- TinocoFrut/financeiro/views.py
+++ TinocoFrut/financeiro/views.py
@@ -4,8 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def comprar(request):
     if request.method == 'POST':
-        # Lógica para realizar uma compra
-        # ...
         return JsonResponse({'message': 'Compra realizada com sucesso'})
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=400)
@@ -13,23 +11,15 @@ def comprar(request):
 @csrf_exempt
 def vender(request):
     if request.method == 'POST':
-        # Lógica para realizar uma venda
-        # ...
         return JsonResponse({'message': 'Venda realizada com sucesso'})
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=400)
 
 def fiscal(request):
-    # Lógica para lidar com aspectos fiscais
-    # ...
     return JsonResponse({'message': 'Aspectos fiscais atualizados'})
 
 def relatorio_compra(request):
-    # Lógica para gerar um relatório de compra
-    # ...
     return JsonResponse({'message': 'Relatório de compra gerado'})
 
 def relatorio_vendas(request):
-    # Lógica para gerar um relatório de vendas
-    # ...
     return JsonResponse({'message': 'Relatório de vendas gerado'})
