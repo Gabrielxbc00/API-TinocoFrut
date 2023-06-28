70cebdec8024aa009250377294e5a87aab951e98
Gabriel Ximenes <117924325+Gabrielxbc00@users.noreply.github.com>
2023-06-28 09:30:05 -0300

    Update models.py

--- TinocoFrut/login/models.py
+++ TinocoFrut/login/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
-    # Outros campos que você possa precisar
 
     def __str__(self):
         return str(self.username)
