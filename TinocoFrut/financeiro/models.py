41b1a500f07e45588ee7eeb805aa96f75ebd6ba0
Gabriel Ximenes <117924325+Gabrielxbc00@users.noreply.github.com>
2023-06-28 09:31:31 -0300

    Update models.py

--- TinocoFrut/financeiro/models.py
+++ TinocoFrut/financeiro/models.py
@@ -4,7 +4,6 @@ class Compra(models.Model):
     fornecedor = models.CharField(max_length=100)
     data = models.DateField()
     valor_total = models.DecimalField(max_digits=10, decimal_places=2)
-    # Outros campos relevantes para a compra
 
     def __str__(self):
         return f"Compra {self.pk}"
@@ -13,7 +12,6 @@ class Venda(models.Model):
     cliente = models.CharField(max_length=100)
     data = models.DateField()
     valor_total = models.DecimalField(max_digits=10, decimal_places=2)
-    # Outros campos relevantes para a venda
 
     def __str__(self):
         return f"Venda {self.pk}"
